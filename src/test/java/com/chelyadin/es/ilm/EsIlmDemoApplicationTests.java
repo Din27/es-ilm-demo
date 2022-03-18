bf08684dc047bea3476ef0659962f62d249cfc4a
Dmytro Chelyadin <dmytro_chelyadin@epam.com>
2022-03-18 02:27:14 +0200

    Initial commit: Added config a main method code for connecting to ES and setting up the ILM, added some simple configs for ILM - ILM policy and index template

---
+++ src/test/java/com/chelyadin/es/ilm/EsIlmDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chelyadin.es.ilm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsIlmDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
